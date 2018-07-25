343d4bf14b6525fd9e7473a58cc4c12a649d5612
zhouchao <zhouchao1105@gmail.com>
2018-07-25 22:44:50 +0800

    将表格数据转为用state控制

--- src/table.js
+++ src/table.js
@@ -1,70 +1,86 @@
 import React from 'react'
 import { Table, Select, Input, Icon } from 'antd'
-const columns = [{
-    title: '中文名',
-    dataIndex: 'name',
-    key: 'name',
-    width: 300
-    // render: text => <a href="javascript:;">{text}</a>,
-},
 
-// {
-//   title: '英文名',
-//   dataIndex: 'eName',
-//   key: 'eName',
-//   width: 300
-
-// }, 
-{
-    title: '历史翻译',
-    dataIndex: 'history',
-    key: 'history',
-    width: 300,
-    render: (item) => {
-        return <Select placeholder="Please Select" style={{ width: '100%' }} defaultActiveFirstOption>
-            {item.map(unit => <Select.Option value={unit}>{unit}</Select.Option>)}
-        </Select>
-    }
-}, {
-    title: '英文名',
-    dataIndex: 'eName',
-    key: 'eName',
-    width: 300,
-    render: (value) => {
-        return <Input
-            value={value}
-            // onChange={this.handleChange}
-            // onPressEnter={this.check}
-            suffix={(
-                <Icon
-                    type="check"
-                // className="editable-cell-icon-check"
-                // onClick={this.check}
-                />
-            )}
-        />
-    }
-
-}, {
-    title: '位置',
-    dataIndex: 'module',
-    key: 'module',
-    width: 100
-
-}, {
-    title: '生效状态',
-    dataIndex: 'status',
-    key: 'status',
-    width: 100
-
-}];
 class MultiTable extends React.Component {
-    componentDidMount() {
-        !this.props.editable && columns.pop()
+    state = {
+        list: []
     }
+    componentDidUpdate() {
+        if (this.props.list.length) {
+            this.setState({
+                list: this.props.list
+            })
+        }
+    }
+    componentDidMount() {
 
+
+        !this.props.editable && this.columns.pop()
+    }
+    columns = [{
+        title: '中文名',
+        dataIndex: 'name',
+        key: 'name',
+        width: 300
+        // render: text => <a href="javascript:;">{text}</a>,
+    },
+    {
+        title: '历史翻译',
+        dataIndex: 'history',
+        key: 'history',
+        width: 300,
+        render: (item, src) => {
+            return <Select placeholder="Please Select" style={{ width: '100%' }} onSelect={(val) => {
+                console.log(src);
+                src.eName = val;
+                this.change(val, src)
+                console.log(src.eName);
+            }}>
+                {item.map(unit => <Select.Option key={unit} value={unit}>{unit}</Select.Option>)}
+            </Select>
+        }
+    }, {
+        title: '英文名',
+        dataIndex: 'eName',
+        key: 'eName',
+        width: 300,
+        render: (value) => {
+            return <Input
+                value={value}
+                // onChange={this.handleChange}
+                // onPressEnter={this.check}
+                suffix={(
+                    <Icon
+                        type="check"
+                    // className="editable-cell-icon-check"
+                    // onClick={this.check}
+                    />
+                )}
+            />
+        }
+
+    }, {
+        title: '位置',
+        dataIndex: 'module',
+        key: 'module',
+        width: 100
+
+    }, {
+        title: '生效状态',
+        dataIndex: 'status',
+        key: 'status',
+        width: 100
+
+    }];
+    change(val, src) {
+        this.setState({
+            list: [src]
+        })
+        // this.props.list.find(unit => unit._id === src._id).eName = val
+        // this.props.list.unshift({ name: 1 })
+    }
     render() {
-        return <Table rowKey="_id" dataSource={this.props.list} columns={columns} pagination={{ position: 'top' }}></Table>
+        return <Table rowKey="_id" dataSource={this.state.list} columns={this.columns} pagination={{ position: 'top' }}></Table>
     }
 }
 export default MultiTable
