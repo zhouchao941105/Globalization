cb203c74ee17c6639045784b3d85c6402afe5213
zhouchao <zhouchao1105@gmail.com>
2018-08-12 16:01:53 +0800

    增加切换到全部翻译tab后的交互和搜索条件

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import MultiTable from './table';
 
 class App extends Component {
   _this = this
-  state = { list: [], totalCount: 0, branchList: [], moduleList: [], selectByBranch: true, defaultBranch: '' }
+  state = { list: [], totalCount: 0, branchList: [], moduleList: [], selectByBranch: true, defaultBranch: '', docType: true }
   componentDidMount() {
     axios.get('/branchList').then(data => {
       this.setState({
@@ -29,8 +29,8 @@ class App extends Component {
         moduleList: data
       })
     })
-    this.getData = ({ branch, module, key, page = { pageIdx: 1, pageSize: 10 } }) => {
-      axios.post('/data', { branch, module, key, page }).then(data => {
+    this.getData = ({ branch, module, key, page = { pageIdx: 1, pageSize: 10 }, state }) => {
+      axios.post('/data', { branch, module, key, page, state }).then(data => {
         this.setState({
           list: data.list,
           totalCount: data.totalCount
@@ -44,6 +44,23 @@ class App extends Component {
       page: {
         pageIdx: 1,
         pageSize: 10
+      },
+      state: '',
+      changeToAllType: function () {
+        //切换到全部翻译
+        this.branch = ''
+        this.module = ''
+        this.key = ''
+        this.page.pageIdx = 1
+        this.state = false
+      },
+      changeToPageType: function () {
+        //切换到页面翻译
+        this.branch = 'v1.0';
+        this.module = '';
+        this.key = '';
+        this.page.pageIdx = 1;
+        this.state = ''
       }
     }
 
@@ -60,6 +77,11 @@ class App extends Component {
       console.log(data);
     })
   }
+  changeDocType(flg) {
+    this.setState({
+      docType: flg
+    })
+  }
   render() {
     return (
       <div className="App">
@@ -73,27 +95,38 @@ class App extends Component {
         {/* <div style={{margin:'0 auto'}}> */}
         <div>
           <Radio.Group defaultValue='页面翻译'>
-            <Radio.Button value='页面翻译'>页面翻译</Radio.Button>
-            <Radio.Button value='翻译总表'>翻译总表</Radio.Button>
+            <Radio.Button value='页面翻译' onClick={() => { this.changeDocType(true); this.searchParam.changeToPageType(); this.getData(this.searchParam) }}>页面翻译</Radio.Button>
+            <Radio.Button value='翻译总表' onClick={() => { this.changeDocType(false); this.searchParam.changeToAllType(); this.getData(this.searchParam) }} >翻译总表</Radio.Button>
           </Radio.Group>
         </div>
         <div>
           <span>筛选：</span>
-          <Radio.Group defaultValue="1" onChange={() => this.setState({ selectByBranch: !this.state.selectByBranch })}>
-            <Radio value="1">按版本</Radio>
-            <Select
-              defaultValue={this.state.defaultBranch}
-              style={{ width: 120 }}
-              onChange={(val) => { this.searchParam.branch = val; this.getData(this.searchParam) }}
-              disabled={!this.state.selectByBranch}>
-              {this.state.branchList.map(item => (<Select.Option key={item} value={item}>{item}</Select.Option>))}
-            </Select>
-            <Radio value="2">按模块</Radio>
-            {/* <Dropdown overlay={ModuleList} trigger={['click']}> */}
-            <Select defaultValue='首页' style={{ width: 120 }} onSelect={(val) => { this.searchParam.module = val; this.getData(this.searchParam) }} disabled={this.state.selectByBranch}>
-              {this.state.moduleList.map(item => (<Select.Option key={item} value={item}>{item}</Select.Option>))}
-            </Select>
-          </Radio.Group>
+          {this.state.docType ?
+            <Radio.Group defaultValue="1" onChange={() => this.setState({ selectByBranch: !this.state.selectByBranch })}>
+              <Radio value="1">按版本</Radio>
+              <Select
+                defaultValue={this.state.defaultBranch}
+                style={{ width: 120 }}
+                onChange={(val) => { this.searchParam.branch = val; this.getData(this.searchParam) }}
+                disabled={!this.state.selectByBranch}>
+                {this.state.branchList.map(item => (<Select.Option key={item} value={item}>{item}</Select.Option>))}
+              </Select>
+              <Radio value="2">按模块</Radio>
+              {/* <Dropdown overlay={ModuleList} trigger={['click']}> */}
+              <Select defaultValue='首页' style={{ width: 120 }} onSelect={(val) => { this.searchParam.module = val; this.getData(this.searchParam) }} disabled={this.state.selectByBranch}>
+                {this.state.moduleList.map(item => (<Select.Option key={item} value={item}>{item}</Select.Option>))}
+              </Select>
+            </Radio.Group> :
+            <div>
+              <Select style={{ width: 120, display: 'inline-block' }} defaultValue="未生效" onSelect={(val) => { this.searchParam.state = val === 0 ? '' : val === 1 ? false : true; this.getData(this.searchParam); }}>
+                <Select.Option key='0'>全部</Select.Option>
+                <Select.Option key='1'>未生效</Select.Option>
+                <Select.Option key='2'>已生效</Select.Option>
+
+              </Select>
+              <Input.Search enterButton style={{ width: 260, marginLeft: 30 }} onSearch={value => { this.searchParam.key = value; this.getData(this.searchParam) }}></Input.Search>
+            </div>
+          }
         </div>
         <div style={{ textAlign: 'left' }}>
           <span>版本/模块:</span>
