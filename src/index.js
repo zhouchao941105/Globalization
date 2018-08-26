8e3970882c4c00d78dc5d1ecce81907ae4827fc4
zhouchao <zhouchao1105@gmail.com>
2018-08-26 20:01:15 +0800

    增加路由，分离登录与主站

--- src/index.js
+++ src/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { Input, Button } from 'antd'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from './net'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class Login extends React.Component {
+    state = {
+        username: '',
+        password: ''
+    }
+    //登录
+    login() {
+        axios.get('/login', {
+            params:
+                {
+                    name: this.state.username,
+                    password: this.state.password
+                }
+        }
+        ).then(res => {
+
+            if (res) {
+
+                window.location.href = "/home"
+            }
+        })
+    }
+    render() {
+        return <div>
+            <div><span>用户名：</span><Input value={this.state.username} style={{ width: 120 }} onChange={(e) => this.setState({ username: e.target.value })} /></div>
+            <div><span>密码：</span><Input style={{ width: 120 }} value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })} /></div>
+            <Button onClick={() => this.login()}>登录</Button>
+        </div>
+    }
+}
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Redirect path="/" exact to="/login"></Redirect>
+
+            <Route path="/home" component={App} />
+            <Route path="/login" component={Login} />
+        </Switch>
+    </Router>, document.getElementById('root'));
 registerServiceWorker();
