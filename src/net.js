0d591203d637131390b0b545f7bf3e4997042587
zhouchao <zhouchao1105@gmail.com>
2018-08-27 23:01:07 +0800

    前端修改对应的错误处理

--- src/net.js
+++ src/net.js
@@ -1,12 +1,11 @@
 import axios from 'axios'
 axios.interceptors.response.use(res => {
-    if (res.data.to) {
-        window.location.href = res.data.to
-    } else {
-        return res.data
-
-    }
+    return res.data
 }, err => {
-    return Promise.reject(err)
+    if (err.response.status === 401) {
+        alert('login')
+        window.location.href = '/login'
+    }
+    return Promise.reject(err.response)
 })
 export default axios
